128611b90f4742bb632592d9f77a79627217a6fa
Plomek <d.cwierz1@gmail.com>
2025-02-16 18:18:02 +0100

    camera zoom

--- Kocmoc/Assets/_Project/Scripts/Gameplay/Camera/CameraDrag.cs
+++ Kocmoc/Assets/_Project/Scripts/Gameplay/Camera/CameraDrag.cs
@@ -8,12 +8,9 @@ namespace Kocmoc.Gameplay
         [SerializeField] private Vector3 offset;
         [SerializeField] private Vector3 bounds;
 
-        private bool dragging;
 
-        private Vector3 dragPosition;
         private Vector3 resetPosition;
-        private Vector3 origin;
-        private Vector3 difference;
+        private Vector2 dragPosition;
         
         private Transform target;
         private Camera cam;
@@ -34,34 +31,25 @@ namespace Kocmoc.Gameplay
         {
             Vector3 targetPosition = target ? target.position : Vector3.zero;
             
-            if(Input.GetMouseButtonDown(1))
+
+            if (Input.GetMouseButton(2))
+            {
+                Vector2 currentMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 previousMousePos = cam.ScreenToWorldPoint(Input.mousePosition - Input.mousePositionDelta);
+                Vector2 difference = currentMousePos - previousMousePos;
+
+                dragPosition -= difference * 2;
+                dragPosition.x = Mathf.Clamp(dragPosition.x, -bounds.x, bounds.x);
+                dragPosition.y = Mathf.Clamp(dragPosition.y, -bounds.y, bounds.y);
+            }
+            else if(Input.GetMouseButtonDown(1))
             {
                 transform.position = resetPosition;
                 dragPosition = Vector3.zero;
                 return;
             }
 
-            if (Input.GetMouseButton(2))
-            {
-                difference = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-                
-                if(!dragging)
-                {
-                    origin = cam.ScreenToWorldPoint(Input.mousePosition);
-                    dragging = true;
-                }
-            }
-            else dragging = false;
-
-
-            if (dragging)
-            {
-                dragPosition = origin - difference - targetPosition;
-                dragPosition.x = Mathf.Clamp(dragPosition.x, -bounds.x, bounds.x);
-                dragPosition.y = Mathf.Clamp(dragPosition.y, -bounds.y, bounds.y);
-            }
-
-            transform.position = targetPosition + dragPosition + offset;
+            transform.position = targetPosition + (Vector3)dragPosition + offset;
         }
     }
 }
--- Kocmoc/Assets/_Project/Scripts/Gameplay/Camera/CameraZoom.cs
+++ Kocmoc/Assets/_Project/Scripts/Gameplay/Camera/CameraZoom.cs
@@ -6,20 +6,31 @@ namespace Kocmoc
     [RequireComponent(typeof(Camera))]
     public class CameraZoom : MonoBehaviour
     {
+        [SerializeField] private float startZoom;
         [SerializeField] private float minZoom;
         [SerializeField] private float maxZoom;
+        [SerializeField] private float zoomSensitivity;
         [SerializeField] private float smoothness;
+
+        private float targetZoom;
 
         private Camera cam;
 
         private void Start()
         {
             cam = GetComponent<Camera>();
+
+            cam.orthographicSize = startZoom;
+            targetZoom = startZoom;
         }
 
         private void Update()
         {
-            
+            targetZoom = targetZoom - Input.mouseScrollDelta.y * zoomSensitivity;
+            targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
+
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, smoothness * Time.deltaTime);
+            if (Mathf.Abs(cam.orthographicSize - targetZoom) < .01f) cam.orthographicSize = targetZoom;
         }
     }
 }
