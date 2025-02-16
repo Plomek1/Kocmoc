6009cd89b90454268320ca36c3d40e516252016a
Plomek <d.cwierz1@gmail.com>
2025-02-16 18:53:13 +0100

    better camera bounding

--- Kocmoc/Assets/_Project/Scripts/Gameplay/Camera/CameraDrag.cs
+++ Kocmoc/Assets/_Project/Scripts/Gameplay/Camera/CameraDrag.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 namespace Kocmoc.Gameplay
@@ -7,7 +8,6 @@ namespace Kocmoc.Gameplay
     {
         [SerializeField] private Vector3 offset;
         [SerializeField] private Vector3 bounds;
-
 
         private Vector3 resetPosition;
         private Vector2 dragPosition;
@@ -30,26 +30,35 @@ namespace Kocmoc.Gameplay
         public void LateUpdate()
         {
             Vector3 targetPosition = target ? target.position : Vector3.zero;
-            
 
-            if (Input.GetMouseButton(2))
-            {
-                Vector2 currentMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 previousMousePos = cam.ScreenToWorldPoint(Input.mousePosition - Input.mousePositionDelta);
-                Vector2 difference = currentMousePos - previousMousePos;
-
-                dragPosition -= difference * 2;
-                dragPosition.x = Mathf.Clamp(dragPosition.x, -bounds.x, bounds.x);
-                dragPosition.y = Mathf.Clamp(dragPosition.y, -bounds.y, bounds.y);
-            }
-            else if(Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonDown(1))
             {
                 transform.position = resetPosition;
                 dragPosition = Vector3.zero;
                 return;
             }
 
+            if (Input.GetMouseButton(2))
+            {
+                Vector2 currentMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 previousMousePos = cam.ScreenToWorldPoint(Input.mousePosition - Input.mousePositionDelta);
+                Vector2 difference = currentMousePos - previousMousePos;
+                dragPosition -= difference * 2;
+            }
+
+            Vector2 screenBottomLeftWorldPos = cam.ScreenToWorldPoint(Vector2.zero) - transform.position;
+            Vector2 screenTopRightWorldPos = cam.ScreenToWorldPoint(new Vector2(cam.pixelWidth, cam.pixelHeight)) - transform.position;
+
+            dragPosition.x = Mathf.Clamp(dragPosition.x, -screenBottomLeftWorldPos.x - bounds.x, -screenTopRightWorldPos.x + bounds.x);
+            dragPosition.y = Mathf.Clamp(dragPosition.y, -screenBottomLeftWorldPos.y - bounds.y, -screenTopRightWorldPos.y + bounds.y);
+
             transform.position = targetPosition + (Vector3)dragPosition + offset;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(target ? target.position : Vector2.zero, bounds * 2);
         }
     }
 }
